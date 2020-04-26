95d12040798d102c3a58b81485b5fbc42e8109ce
kevinnayar <kevin.nayar@gmail.com>
2020-04-26 00:58:23 -0500

    auth logic

--- server/api/api.ts
+++ server/api/api.ts
@@ -2,9 +2,10 @@ import { Application } from 'express';
 import * as _pgPromise from 'pg-promise';
 import { IMain, IDatabase } from 'pg-promise';
 
-import _userQueries from './entities/user/userQueries';
-import _userCommands from './entities/user/userCommands';
+import _auth from './auth/auth';
 import _plantQueries from './entities/plant/plantQueries';
+import _userCommands from './entities/user/userCommands';
+import _userQueries from './entities/user/userQueries';
 import _zoneQueries from './entities/zone/zoneQueries';
 
 import { TypeDBConfig } from '../../types/baseTypes';
@@ -12,17 +13,19 @@ import { TypeDBConfig } from '../../types/baseTypes';
 export const register = (app: Application, config: TypeDBConfig) => {
   const pgPromise: IMain = _pgPromise();
   const db: IDatabase<{}, any> = pgPromise(config);
-  
-  const userQueries = _userQueries(db);
-  app.get(`/api/users/:userId`, userQueries.getUserById);
-  app.get(`/api/users/:userId/plants`, userQueries.getPlantsByUserId);
 
-  const userCommands = _userCommands(db);
-  app.post(`/api/users/:userId`, userCommands.getUserById);
-
+  const auth = _auth(db);
   const plantQueries = _plantQueries(db);
-  app.get(`/api/plants/:plantId`, plantQueries.getPlantById);
-
+  const userCommands = _userCommands(db);
+  const userQueries = _userQueries(db);
   const zoneQueries = _zoneQueries(db);
+
+  // unauthenticated
   app.get(`/api/zones/:zipcode`, zoneQueries.getZoneByZipcode);
+
+  // authenticated
+  app.get(`/api/plants/:plantId`, auth.validateUser, plantQueries.getPlantById);
+  app.get(`/api/users/:userId`, auth.validateUser, userQueries.getUserById);
+  app.get(`/api/users/:userId/plants`, auth.validateUser, userQueries.getPlantsByUserId);
+  app.post(`/api/users/:userId`, auth.validateUser, userCommands.getUserById);
 };
---
+++ server/api/auth/auth.ts
@@ -0,0 +1,70 @@
+import * as jsonwebtoken from 'jsonwebtoken';
+import * as jwksClient from 'jwks-rsa';
+import { Request, Response } from 'express';
+import { IDatabase } from 'pg-promise';
+
+import config from '../../../client/config';
+import { apiFormatError, strictStringOrThrow, trueOrThrow } from '../../../utils/apiUtils';
+import { UnauthorizedException } from '../../../utils/exceptionUtils';
+
+const region = config.cognito.AWS_REGION;
+const userpoolId = config.cognito.AWS_COGNITO_POOL_ID;
+const keysUrlPrefix = `https://cognito-idp.${region}.amazonaws.com/${userpoolId}`;
+const keysUrl = `${keysUrlPrefix}/.well-known/jwks.json`;
+
+interface DecodedBody {
+  sub: string;
+  exp: number;
+  iss: string;
+  username: string;
+}
+
+const client = jwksClient({
+  cache: true,
+  jwksUri: keysUrl,
+});
+
+function getKey(header: jsonwebtoken.JwtHeader, callback: jsonwebtoken.SigningKeyCallback) {
+  client.getSigningKey(header.kid!, (err: Error, key: jwksClient.SigningKey) => {
+    if (err) return callback(err);
+    callback(null, key.getPublicKey());
+  });
+}
+
+function isTokenExpired(exp: number): boolean {
+  const now = new Date();
+  const seconds = Math.floor(now.getTime() / 1000);
+  return exp < seconds;
+}
+
+export default function (db: IDatabase<{}, any>) {
+  async function validateUser(req: Request, res: Response, next: Function) {
+    try {
+      const token = strictStringOrThrow(
+        req.headers.authorization,
+        'Authorization header is invalid or non-existent.',
+      );
+      const jwtToken = strictStringOrThrow(
+        token.replace('Bearer ', ''),
+        'JWT Token is required and cannot be empty.',
+      );
+      const payload: DecodedBody = await new Promise((resolve, reject) => {
+        jsonwebtoken.verify(jwtToken, getKey, (err: Error, decoded) => {
+          if (err) return reject(err);
+          resolve(<DecodedBody>decoded);
+        });
+      });
+
+      trueOrThrow(!isTokenExpired(payload.exp), 'Token is expired.');
+      trueOrThrow(payload.iss === keysUrlPrefix, 'Token issuer does not match client.');
+      next();
+    } catch (err) {
+      return res.status(401).json(UnauthorizedException(apiFormatError(err)));
+    }
+  }
+
+  return {
+    validateUser,
+  };
+}
+
